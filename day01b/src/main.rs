07de78acb1723c771bb46f30d1a9e55e9f813ff3
Justin <justinluong1@gmail.com>
2023-12-02 22:13:45 +1100

    completed 1b

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,7 +1,7 @@
+use std::collections::{HashMap, HashSet};
+use std::env;
 use std::fs;
-use std::collections::HashMap;
 use std::path::Path;
-use regex::Regex;
 
 fn create_numeric_map() -> HashMap<&'static str, &'static str> {
     let mut map = HashMap::new();
@@ -18,42 +18,66 @@ fn create_numeric_map() -> HashMap<&'static str, &'static str> {
     map
 }
 
+fn digit_from_match(digit_match: &str, map: &HashMap<&'static str, &'static str>) -> String {
+    match map.get(digit_match) {
+        Some(&digit) => digit.to_string(),
+        None => digit_match.to_string(),
+    }
+}
+
+fn find_matches(line: &str) -> Vec<String> {
+    let digit_words: HashSet<&str> = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .iter()
+    .cloned()
+    .collect();
+    let max_length = digit_words.iter().map(|word| word.len()).max().unwrap();
+
+    let mut matches = Vec::new();
+
+    for i in 0..line.len() {
+        for j in i + 1..=line.len() {
+            let current_str = &line[i..j];
+            if current_str.len() == 1 && current_str.chars().next().unwrap().is_digit(10)
+                || digit_words.contains(current_str)
+            {
+                matches.push(current_str.to_string());
+                break;
+            } else if current_str.len() > max_length {
+                break;
+            }
+        }
+    }
+
+    matches
+}
+
 fn extract_digits(line: &str, map: &HashMap<&'static str, &'static str>) -> u32 {
-    let pattern = r"one|two|three|four|five|six|seven|eight|nine|\d";
-    let re = Regex::new(pattern).unwrap();
-    let matches: Vec<_> = re.captures_iter(line).collect();
-    let first_match: &str = matches.first().unwrap().get(0).unwrap().as_str();
-
-    let mut first_digit = "";
-    let mut last_digit = "";
-
-    if first_match.len() != 1 {
-        first_digit = map.get(first_match).unwrap();
-    } else {
-        first_digit = first_match;
-    }
-    let last_match: &str = matches.last().unwrap().get(0).unwrap().as_str();
-    if last_match.len() != 1 {
-        last_digit = map.get(last_match).unwrap();
-    } else {
-        last_digit = last_match;
-    }
-
-    println!("{}", first_digit);
-    println!("{}", last_digit);
-    1
+    let matches: Vec<String> = find_matches(line);
+    let first_digit = digit_from_match(&matches.first().unwrap(), map);
+    let last_digit = digit_from_match(&matches.last().unwrap(), map);
+    (first_digit + &last_digit).parse::<u32>().unwrap()
 }
 
 fn main() {
-    let file_path = Path::new("./input.txt");
+    let args: Vec<String> = env::args().collect();
+    let file_path_str = args
+        .get(1)
+        .cloned()
+        .unwrap_or_else(|| "input.txt".to_string());
+    let file_path = Path::new(&file_path_str);
+
     let contents = fs::read_to_string(file_path).unwrap();
     let lines: Vec<&str> = contents.lines().collect();
 
     let map = create_numeric_map();
+    let calibration_values: Vec<u32> = lines
+        .iter()
+        .map(|line| extract_digits(line, &map))
+        .collect();
 
-    for line in lines {
-        let extracted_digits = extract_digits(line, &map);
-        println!("{}", extracted_digits);
-        break
-    }
+    let sum: u32 = calibration_values.iter().map(|&x| x).sum();
+
+    println!("Sum: {}", sum);
 }
